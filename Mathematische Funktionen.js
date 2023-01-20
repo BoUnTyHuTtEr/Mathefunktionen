f389f5e54eeaa163d7c75a9abb046a72946f4a64
BoUnTyHuTtEr <113713840+BoUnTyHuTtEr@users.noreply.github.com>
2023-01-20 18:03:13 +0100

    Update Mathematische Funktionen.js

--- Mathematische Funktionen.js
+++ Mathematische Funktionen.js
@@ -1,7 +1,7 @@
-
+"use strict"
 function Nstlfunction () {
-    let   m = ((document.getElementById("Datainput1")||{}).value)||"";
-    let   t = ((document.getElementById("Datainput2")||{}).value)||"";
+let   m = ((document.getElementById("Datainput1")||{}).value)||"";
+let   t = ((document.getElementById("Datainput2")||{}).value)||"";
 if (document.getElementById("Datainput1").value.length == 0 && document.getElementById("Datainput2").value.length == 0  ) {
 console.log("Error");
 setTimeout(() => {document.getElementById("Datainput1").style.borderColor = "Red"},500)
@@ -25,12 +25,32 @@ else {
 let extratext = "|0)"    
 let xkor = 
 (0 -t)/(m) 
-console.log(xkor)
+console.log( "Die Nullstelle liegt bei " + xkor)
+let xkorrounded = Math.round(xkor *100) /100
 let Nullstelle =  document.getElementById("Nullestelle");
-Nullstelle.innerHTML = "Die Nullstelle liegt bei S(" + xkor + extratext
+Nullstelle.innerHTML = "Die Nullstelle liegt bei S(" + xkorrounded + extratext
 Nullstelle.style.display = "inline"
-
-    
-
 }
 }
+function Lfunctioninfo () {
+let   m = ((document.getElementById("Datainput1")||{}).value)||"";
+let infostring1 = "Die Gerade ist steigend" 
+let infostring2 = "Die Gerade ist fallend"
+let infostring3 = "Die Gerade ist gerade"
+let Infotext = document.getElementById("lfinfo"); 
+if (m > 0 ) {
+console.log(infostring1)
+Infotext.innerHTML = infostring1
+Infotext.style.display = "inline"
+} else if (m < 0) {
+console.log(infostring2)
+Infotext.innerHTML = infostring2
+Infotext.style.display = "inline"    
+}
+else {
+console.log(infostring3)
+Infotext.innerHTML = infostring3
+Infotext.style.display = "inline"   
+}       
+}
+
